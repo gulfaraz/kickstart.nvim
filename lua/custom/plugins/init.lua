9540bdd0f89bb10b767f90997bbcc86c0ae85b83
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-04-24 12:29:07 +0200

    plugin: move neoscroll and smear-cursor to separate files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -196,18 +196,4 @@ return {
       require('lsp-file-operations').setup()
     end,
   },
-  {
-    'karb94/neoscroll.nvim',
-    opts = {},
-  },
-  {
-    'sphamba/smear-cursor.nvim',
-    opts = {
-      stiffness = 0.55, -- 0.6      [0, 1]
-      trailing_stiffness = 0.5, -- 0.4      [0, 1]
-      stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
-      trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
-      distance_stop_animating = 0.5, -- 0.1      > 0
-    },
-  },
 }
