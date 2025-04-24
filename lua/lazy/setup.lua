3e6a9fe6e39a791ff88531e137e8688b13115538
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-04-24 19:38:46 +0200

    plugin: which-key

--- lua/lazy/setup.lua
+++ lua/lazy/setup.lua
@@ -2,62 +2,6 @@ require('lazy').setup(
   {
     'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-    { -- Useful plugin to show you pending keybinds.
-      'folke/which-key.nvim',
-      event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-      config = function() -- This is the function that runs, AFTER loading
-        require('which-key').setup {
-          icons = {
-            -- set icon mappings to true if you have a Nerd Font
-            mappings = vim.g.have_nerd_font,
-            -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-            -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
-            keys = vim.g.have_nerd_font and {} or {
-              Up = '<Up> ',
-              Down = '<Down> ',
-              Left = '<Left> ',
-              Right = '<Right> ',
-              C = '<C-…> ',
-              M = '<M-…> ',
-              D = '<D-…> ',
-              S = '<S-…> ',
-              CR = '<CR> ',
-              Esc = '<Esc> ',
-              ScrollWheelDown = '<ScrollWheelDown> ',
-              ScrollWheelUp = '<ScrollWheelUp> ',
-              NL = '<NL> ',
-              BS = '<BS> ',
-              Space = '<Space> ',
-              Tab = '<Tab> ',
-              F1 = '<F1>',
-              F2 = '<F2>',
-              F3 = '<F3>',
-              F4 = '<F4>',
-              F5 = '<F5>',
-              F6 = '<F6>',
-              F7 = '<F7>',
-              F8 = '<F8>',
-              F9 = '<F9>',
-              F10 = '<F10>',
-              F11 = '<F11>',
-              F12 = '<F12>',
-            },
-          },
-        }
-
-        -- Document existing key chains
-        require('which-key').add {
-          { '<leader>c', group = '[c]ode' },
-          { '<leader>l', group = '[l]sp' },
-          { '<leader>r', group = '[r]ename' },
-          { '<leader>s', group = '[s]earch' },
-          { '<leader>w', group = '[w]orkspace' },
-          { '<leader>t', group = '[t]oggle' },
-          { '<leader>g', group = '[g]it', mode = { 'n', 'v' } },
-        }
-      end,
-    },
-
     { -- Fuzzy Finder (files, lsp, etc)
       'nvim-telescope/telescope.nvim',
       event = 'VimEnter',
