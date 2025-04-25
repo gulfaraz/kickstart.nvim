e7f93cd3a34eec71e49d569c4bacc5146b04ca89
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-04-25 15:53:23 +0200

    configure: lazygit with nvim server

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,5 @@
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'exit terminal mode' })
+
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'diagnostic [q]uickfix list' })
--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -15,4 +15,35 @@ return {
   keys = {
     { '|', '<CMD>LazyGit<CR>', desc = 'lazygit' },
   },
+  init = function()
+    -- create an alias for nvim in .zshrc
+    -- alias nvim="nvim --listen /tmp/nvim-server.pipe"
+    --
+    -- update lazygit config file ~/Library/Application\ Support/lazygit/config.yml
+    --
+    -- quitOnTopLevelReturn: false
+    --
+    -- os:
+    --   edit: '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server $NVIM --remote-send ''<CMD>q<CR><CMD>lua EditFromLazygit({{filename}})<CR>'')'
+    --   editAtLine: '[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || nvim --server $NVIM --remote-send ''<CMD>q<CR><CMD>lua EditLineFromLazygit({{filename}},{{line}})<CR>'''
+
+    function EditLineFromLazygit(file_path, line)
+      local path = vim.fn.expand '%:p'
+      if path == file_path then
+        vim.cmd(tostring(line))
+      else
+        vim.cmd('e ' .. file_path)
+        vim.cmd(tostring(line))
+      end
+    end
+
+    function EditFromLazygit(file_path)
+      local path = vim.fn.expand '%:p'
+      if path == file_path then
+        return
+      else
+        vim.cmd('e ' .. file_path)
+      end
+    end
+  end,
 }
