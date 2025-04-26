7fe292b2e1517389f615e2d56347807f3033c4a2
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-04-26 23:18:01 +0200

    style: remove redundant comment

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -33,8 +33,5 @@ return {
     statusline.section_location = function()
       return '%2l:%-2v'
     end
-
-    -- ... and there is more!
-    --  Check out: https://github.com/echasnovski/mini.nvim
   end,
 }
