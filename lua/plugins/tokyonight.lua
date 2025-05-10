8a2081e85a82edc7242323396efc21dad3449480
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2025-05-10 23:01:04 +0200

    config: toggle colourscheme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -12,7 +12,22 @@ return {
   },
   init = function()
     vim.cmd.colorscheme 'tokyonight-night'
-
     vim.api.nvim_set_hl(0, 'ColorColumn', { ctermbg = 0, bg = '#292e42' })
   end,
+  keys = {
+    {
+      '<leader>tc',
+      mode = 'n',
+      function()
+        if vim.g.colors_name == 'tokyonight-night' then
+          vim.cmd.colorscheme 'tokyonight-day'
+          vim.api.nvim_set_hl(0, 'ColorColumn', { ctermbg = 0, bg = '#c4c8da' })
+        else
+          vim.cmd.colorscheme 'tokyonight-night'
+          vim.api.nvim_set_hl(0, 'ColorColumn', { ctermbg = 0, bg = '#292e42' })
+        end
+      end,
+      desc = '[t]oggle [c]olourscheme',
+    },
+  },
 }
