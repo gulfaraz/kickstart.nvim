0555494aaa72dc86cedc39d6a313724dac9dba27
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2026-02-19 12:51:11 +0100

    config: extend alternate toggler

--- lua/plugins/alternate-toggler.lua
+++ lua/plugins/alternate-toggler.lua
@@ -4,6 +4,14 @@
 return {
   'rmagatti/alternate-toggler',
   event = { 'BufReadPost' },
+  opts = {
+    alternates = {
+      ['private'] = 'public',
+      ['enable'] = 'disable',
+      ['key'] = 'value',
+      ['const'] = 'let',
+    },
+  },
   keys = {
     { '<leader>ta', mode = 'n', '<CMD>ToggleAlternate<CR>', desc = '[t]oggle [a]lternate' },
   },
